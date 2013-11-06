bc8a67dff4dff03f71c6b736d93b62729bfe88f6
slavara <ratdg@ya.ru>
2013-11-06 22:59:02 +0400

    -- добавил пустые тесты под проект.

---
+++ test/match3/core/controllers/Math3ControllerTest.hx
@@ -0,0 +1,12 @@
+package match3.core.controllers;
+
+/**
+ * @author SlavaRa
+ */
+class Math3ControllerTest {
+
+	public function new() {
+	}
+	
+	//TODO: implement me!
+}
---
+++ test/match3/core/models/Match3ModelTest.hx
@@ -0,0 +1,12 @@
+package match3.core.models;
+
+/**
+ * @author SlavaRa
+ */
+class Match3ModelTest {
+
+	public function new() {
+	}
+	
+	//TODO: implement me!
+}
