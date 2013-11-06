fe245b275d36d6e1337e7b85893dda740080a78a
slavara <ratdg@ya.ru>
2013-11-06 23:32:21 +0400

    -- Math3View -> Match3View.

---
+++ src/match3/core/display/Match3View.hx
@@ -0,0 +1,11 @@
+package match3.core.display;
+import slavara.haxe.core.display.DisplayObject.ResourceSprite;
+
+/**
+ * @author SlavaRa
+ */
+class Match3View extends ResourceSprite {
+
+	public function new() super();
+	
+}
