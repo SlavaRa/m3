edd1580b6083241f30adf27b344d5542131a186f
slavara <ratdg@ya.ru>
2013-11-17 20:07:32 +0400

    -- add ServerController.

---
+++ src/match3/core/controllers/ServerController.hx
@@ -0,0 +1,13 @@
+package match3.core.controllers;
+import slavara.haxe.core.controllers.BaseController.AbstractController;
+
+/**
+ * @author SlavaRa
+ */
+class ServerController extends AbstractController {
+
+	public function new() super(this);
+	
+	public override function initialize() {
+	}
+}
