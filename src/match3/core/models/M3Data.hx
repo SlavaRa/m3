0046b29891e0f1e72b6612fe59a96bae0a365f9f
slavara <ratdg@ya.ru>
2014-01-31 16:33:22 +0400

    -- refactor.

--- src/match3/core/models/M3Data.hx
+++ src/match3/core/models/M3Data.hx
@@ -1,37 +1,15 @@
 package match3.core.models;
-import match3.core.Events.M3DataEvent;
-import match3.core.Interfaces.IBehavior;
-import match3.core.models.behaviors.BehaviorCollectionData;
 import slavara.haxe.core.Models.DataValueObjectContainer;
-using slavara.haxe.core.utils.Utils.ValidateUtil;
 
 /**
  * @author SlavaRa
  */
 class M3Data extends DataValueObjectContainer {
-
+	
 	public function new() {
 		super();
-		_behaviors = new BehaviorCollectionData();
+		addChild(user = new UserData());
 	}
 	
-	var _behaviors:BehaviorCollectionData;
-	var _behavior:IBehavior;
-	
-	public function setBehavior(type:BehaviorType) {
-		var behavior = _behaviors.getBehavior(type);
-		if(behavior == _behavior) {
-			return;
-		}
-		if(_behavior.isNotNull()) {
-			_behavior.reset();
-		}
-		_behavior = behavior;
-		_behavior.start();
-		if(willTrigger(M3DataEvent.BEHAVIOR_CHANE)) {
-			dispatchEvent(new M3DataEvent(M3DataEvent.BEHAVIOR_CHANE, true));
-		}
-	}
-	
-	
+	public var user(default, null):UserData;
 }
