a0e0f1c320be81b9582cd1fc7ec9fcc967507fba
slavara <ratdg@ya.ru>
2014-01-31 14:56:25 +0400

    extends DataContainer -> extends DataValueObjectContainer

--- src/match3/core/models/UnitData.hx
+++ src/match3/core/models/UnitData.hx
@@ -1,11 +1,11 @@
 package match3.core.models;
 import match3.core.Interfaces.IUnknown;
-import slavara.haxe.core.models.Data.DataContainer;
+import slavara.haxe.core.Models.DataValueObjectContainer;
 
 /**
  * @author SlavaRa
  */
-class UnitData extends DataContainer implements IUnknown {
+class UnitData extends DataValueObjectContainer implements IUnknown {
 
 	public function new() super();
 	
