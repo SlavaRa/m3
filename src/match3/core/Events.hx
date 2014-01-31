d56ae16d27fd1b20484f9baa1846c832107e12b0
slavara <ratdg@ya.ru>
2014-01-31 19:07:09 +0400

    -- M3DataEvent -> DataEvent -- add ControllerEvent

--- src/match3/core/Events.hx
+++ src/match3/core/Events.hx
@@ -1,16 +1,25 @@
 package match3.core;
 import flash.events.Event;
-import match3.core.Events.M3DataEvent;
+import match3.core.Events.ControllerEvent;
+import match3.core.Events.DataEvent;
 import slavara.haxe.core.events.models.DataBaseEvent;
 
 /**
  * @author SlavaRa
  */
-class M3DataEvent extends DataBaseEvent {
+class DataEvent extends DataBaseEvent {
 	
 	public static inline var BEHAVIOR_CHANE = "behaviorChane";
 	
-	public function new(type:String, bubbles:Bool = false, cancelable:Bool = false)	super(type, bubbles, cancelable);
+	public function new(type:String, ?bubbles:Bool, ?cancelable:Bool) super(type, bubbles, cancelable);
+}
+
+/**
+ * @author SlavaRa
+ */
+class ControllerEvent extends Event {
 	
-	public override function clone():Event return new M3DataEvent(type, bubbles, cancelable);
+	public function new(type:String, ?cancelable:Bool) super(type, false, cancelable);
+	
+	public override function clone():Event return new ControllerEvent(type, cancelable);
 }
