addad6186c91f2a9b5960bc3d4c0213756bff1f5
slavara <ratdg@ya.ru>
2014-02-02 17:48:06 +0400

    -- deserialize id, desc.

--- src/match3/core/models/UnitData.hx
+++ src/match3/core/models/UnitData.hx
@@ -1,6 +1,7 @@
 package match3.core.models;
 import match3.core.Interfaces.IUnknown;
 import slavara.haxe.core.Models.DataValueObjectContainer;
+using Reflect;
 
 /**
  * @author SlavaRa
@@ -11,4 +12,15 @@ class UnitData extends DataValueObjectContainer implements IUnknown {
 	
 	public var id(default, default):Int;
 	public var desc(default, default):String;
+	
+	private override function deserialize(input:Dynamic) {
+		super.deserialize(input);
+		
+		if(input.hasField("id")) {
+			id = input.getProperty("id");
+		}
+		if(input.hasField("desc")) {
+			desc = input.getProperty("desc");
+		}
+	}
 }
