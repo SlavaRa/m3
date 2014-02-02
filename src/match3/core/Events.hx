effb6160512da2d30177fb9a3611e1f967789216
slavara <ratdg@ya.ru>
2014-02-02 20:42:02 +0400

    -- add GameEvent.GOTO_LOCATION_SCREEN

--- src/match3/core/Events.hx
+++ src/match3/core/Events.hx
@@ -47,6 +47,7 @@ class GameEvent extends Event {
 	public static inline var RESET = "reset";
 	public static inline var GOTO_EMPTY_SCREEN = "gotoEmptyScreen";
 	public static inline var GOTO_WORLD_SCREEN = "gotoWorldScreen";
+	public static inline var GOTO_LOCATION_SCREEN = "gotoLocationScreen";
 	
 	public function new(type:String, ?bubbles:Bool, ?cancelable:Bool) super(type, bubbles, cancelable);
 	
