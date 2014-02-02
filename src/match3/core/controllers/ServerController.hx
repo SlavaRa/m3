cee49210022995fc940fc3965881188fdb23b7b9
slavara <ratdg@ya.ru>
2014-02-02 20:37:20 +0400

    -- cleanup.

--- src/match3/core/controllers/ServerController.hx
+++ src/match3/core/controllers/ServerController.hx
@@ -10,10 +10,6 @@ class ServerController extends AbstractController {
 
 	public function new(controller:IController) super(controller);
 	
-	public override function initialize() {
-	}
-	
-	public function send(command:ServerCommand, data:Dynamic, onResponseReceived:Dynamic->Void, onResponseSavedData:Dynamic) {
-		
+	public function send(command:ServerCommand, ?data:Dynamic, ?onResponseReceived:Dynamic->Void, ?onResponseSavedData:Dynamic) {
 	}
 }
