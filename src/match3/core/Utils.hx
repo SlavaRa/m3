34c5e3353018eecd8eee14e96919ef9043ef1078
slavara <ratdg@ya.ru>
2014-02-02 19:46:08 +0400

    -- add public static inline function getServiceButtonFormat():TextFormat

--- src/match3/core/Utils.hx
+++ src/match3/core/Utils.hx
@@ -8,4 +8,8 @@ extern class TextFieldUtils {
 	public static inline function getServiceTextFormat():TextFormat {
 		return new TextFormat("Arial", 16, 0x0080FF, false, false, false, null, null, null, 0, 0, 0, 0);
 	}
+	
+	public static inline function getServiceButtonFormat():TextFormat {
+		return new TextFormat("Arial", 12, 0x00AE00, false, false, false, null, null, null, 0, 0, 0, 0);
+	}
 }
