2865da2dbb1fd3f34b2efed01a8161dfae389adb
slavara <ratdg@ya.ru>
2014-02-03 16:53:34 +0400

    -- add world field.

--- src/match3/core/DataBase.hx
+++ src/match3/core/DataBase.hx
@@ -2,6 +2,7 @@ package match3.core;
 import match3.core.Enums.GameState;
 import match3.core.models.UnitData;
 import match3.core.models.UserData;
+import match3.core.models.WorldData;
 import slavara.haxe.core.StateMachine;
 using Reflect;
 
@@ -13,6 +14,7 @@ class DataBase extends UnitData {
 	public function new() super();
 	
 	public var stateMachine(default, null):StateMachine;
+	public var world(default, null):WorldData;
 	public var user(default, null):UserData;
 	
 	override function initialize() {
@@ -21,11 +23,16 @@ class DataBase extends UnitData {
 		stateMachine = new StateMachine();
 		stateMachine.setState(GameState.Empty);
 		
+		addChild(world = new WorldData());
 		addChild(user = new UserData());
 	}
 	
 	override function deserialize(input:Dynamic) {
 		super.deserialize(input);
+		
+		if(input.hasField("world")) {
+			world.readExternal(input.getProperty("world"));
+		}
 		
 		if(input.hasField("user")) {
 			user.readExternal(input.getProperty('user'));
