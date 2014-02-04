cf3637786271424c56c1ef156bc317055c1d2581
slavara <ratdg@ya.ru>
2014-02-04 17:34:00 +0400

    -- сохраняем линк DataBase.

--- src/match3/core/models/UnitData.hx
+++ src/match3/core/models/UnitData.hx
@@ -1,15 +1,19 @@
 package match3.core.models;
+import match3.core.DataBase;
 import match3.core.Interfaces.IUnknown;
+import slavara.haxe.core.events.models.DataBaseEvent;
 import slavara.haxe.core.Models.DataValueObjectContainer;
 import slavara.haxe.core.StateMachine;
+using slavara.haxe.core.utils.Utils.ValidateUtil;
 using Reflect;
+using Std;
 
 /**
  * @author SlavaRa
  */
 class UnitData extends DataValueObjectContainer implements IUnknown {
 
-	public function new() {
+	function new() {
 		super();
 		initialize();
 	}
@@ -17,9 +21,12 @@ class UnitData extends DataValueObjectContainer implements IUnknown {
 	public var stateMachine(default, null):StateMachine;
 	public var id(default, default):Int;
 	public var desc(default, default):String;
+	var _database:DataBase;
 	
 	function initialize() {
 		stateMachine = new StateMachine();
+		addEventListener(DataBaseEvent.ADDED, onAdded);
+		addEventListener(DataBaseEvent.REMOVED, onRemoved);
 	}
 	
 	override function deserialize(input:Dynamic) {
@@ -31,5 +38,26 @@ class UnitData extends DataValueObjectContainer implements IUnknown {
 		if(input.hasField("desc")) {
 			desc = input.getProperty("desc");
 		}
+	}
+	
+	@:final @:noCompletion function onAdded(event:DataBaseEvent) {
+		if(event.currentTarget != event.target) {
+			return;
+		}
+		var parent = this.parent;
+		while(parent.isNotNull()) {
+			if(parent.is(DataBase)) {
+				_database = cast(parent, DataBase);
+				break;
+			}
+			parent = parent.parent;
+		}
+	}
+	
+	@:final @:noCompletion function onRemoved(event:DataBaseEvent) {
+		if(event.currentTarget != event.target) {
+			return;
+		}
+		_database = null;
 	}
 }
