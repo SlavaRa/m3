f40f3cc576dbecd925b0e475b75cb3663bf14f5f
slavara <ratdg@ya.ru>
2014-02-04 16:17:24 +0400

    -- add PrototypesCollectionData.

---
+++ src/match3/core/models/PrototypesCollectionData.hx
@@ -0,0 +1,12 @@
+package match3.core.models;
+import slavara.haxe.core.Models.DataValueObjectContainer;
+
+/**
+ * @author SlavaRa
+ */
+@:final class PrototypesCollectionData extends DataValueObjectContainer {
+	
+	public function new() {
+		super();
+	}
+}
