5bab7ebaf162cf0c7b0e9e0a182a7370250a4b3b
slavara <ratdg@ya.ru>
2014-02-05 15:48:48 +0400

    -- add @:noCompletion inline function deserializeLocations(input:Dynamic)

--- src/match3/core/models/prototypes/WorldProto.hx
+++ src/match3/core/models/prototypes/WorldProto.hx
@@ -1,4 +1,7 @@
 package match3.core.models.prototypes;
+import slavara.haxe.core.Models.Data;
+using Reflect;
+using Std;
 
 /**
  * @author SlavaRa
@@ -6,4 +9,40 @@ package match3.core.models.prototypes;
 class WorldProto extends LocationProto {
 	
 	public function new() super();
+	
+	public var id2location(default, null):Map<Int, LocationProto>;
+	
+	override function initialize() {
+		super.initialize();
+		id2location = new Map();
+	}
+	
+	override function deserialize(input:Dynamic) {
+		super.deserialize(input);
+		deserializeLocations(input);
+	}
+	
+	@:noCompletion inline function deserializeLocations(input:Dynamic) {
+		if(input.hasField("+locations")) {
+			var locations:Array<Dynamic> = input.getProperty("+locations");
+			for(it in locations) {
+				var location = new LocationProto();
+				location.readExternal(it);
+				addChild(location);
+			}
+		}
+	}
+	
+	override function addChildBefore(child:Data) {
+		super.addChildBefore(child);
+		
+		if(child.is(LocationData)) {
+			var location:LocationProto = cast(child, LocationProto);
+			var id = location.id;
+			if(id2location.exists(id)) {
+				//TODO: throw error
+			}
+			id2location.set(id, location);
+		}
+	}
 }
