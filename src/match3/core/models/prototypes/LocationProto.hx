79fe2547311d58086df0bc19f08fa702759abf0f
slavara <ratdg@ya.ru>
2014-02-05 17:16:23 +0400

    -- add override function deserialize(input:Dynamic)

--- src/match3/core/models/prototypes/LocationProto.hx
+++ src/match3/core/models/prototypes/LocationProto.hx
@@ -6,4 +6,18 @@ package match3.core.models.prototypes;
 class LocationProto extends UnitProto {
 
 	public function new() super();
+	
+	override function deserialize(input:Dynamic) {
+		super.deserialize(input);
+		
+		if(input.getProperty("behavior")) {
+			//TODO: implement me
+		}
+		if(input.getProperty("units")) {
+			//TODO: implement me
+		}
+		if(input.getProperty("rewards")) {
+			//TODO: implement me
+		}
+	}
 }
