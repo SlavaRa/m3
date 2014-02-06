6484ef09d18cff5dc4f927b4b20c82d582569d65
slavara <ratdg@ya.ru>
2014-02-06 18:36:04 +0400

    -- refactor @:noCompletion var _id2location:Map<Int, LocationData> -> var _locations:DataCollection<LocationData>

--- src/match3/core/models/WorldData.hx
+++ src/match3/core/models/WorldData.hx
@@ -1,94 +1,24 @@
 package match3.core.models;
-import flash.errors.ArgumentError;
+import match3.core.models.prototypes.UnknownProto;
 import match3.core.models.prototypes.WorldProto;
-import slavara.haxe.core.Models.Data;
-using slavara.haxe.core.utils.Utils.ValidateUtil;
-using Reflect;
-using Std;
 
 /**
  * @author SlavaRa
  */
 class WorldData extends LocationData {
 
-	public function new(proto:WorldProto) super(proto);
+	public function new(proto:UnknownProto) super(proto);
 	
-	@:noCompletion var _id2location:Map<Int, LocationData>;
+	var _locations:DataCollection<LocationData>;
 	
 	override function initialize() {
 		super.initialize();
-		_id2location = new Map();
+		var proto:WorldProto = cast(_proto, WorldProto);
+		_locations = new DataCollection<LocationData>(proto.locations, "locations");
 	}
 	
 	override function deserialize(input:Dynamic) {
 		super.deserialize(input);
-		deserializeLocations(input);
-	}
-	
-	@:noCompletion inline function deserializeLocations(input:Dynamic) {
-		if(input.hasField("+locations")) {
-			var proto:WorldProto = cast(_proto, WorldProto);
-			var locations:Array<Dynamic> = input.getProperty("+locations");
-			for(it in locations) {
-				if(it.hasField("proto_id")) {
-					var protoId:Int = it.getProperty("proto_id");
-					var location = new LocationData(proto.locations.get(protoId));
-					location.readExternal(it);
-					addChild(location);
-				} else {
-					//TODO: throw error
-				}
-			}
-		}
-		if(input.hasField("-locations")) {
-			var locations:Array<Dynamic> = input.getProperty("-locations");
-			for(it in locations) {
-				if(it.hasField("id")) {
-					var id:Int = it.getProperty("id");
-					if(_id2location.exists(id)) {
-						removeChild(_id2location.get(id));
-					}
-				} else {
-					//TODO: throw error
-				}
-			}
-		}
-		if(input.hasField("=locations")) {
-			var locations:Array<Dynamic> = input.getProperty("=locations");
-			for(it in locations) {
-				if(it.hasField("id")) {
-					var id:Int = it.getProperty("id");
-					if(_id2location.exists(id)) {
-						_id2location.get(id).readExternal(it);
-					}
-				} else {
-					//TODO: throw error
-				}
-			}
-		}
-	}
-	
-	override function addChildBefore(child:Data) {
-		super.addChildBefore(child);
-		
-		if(child.is(LocationData)) {
-			var location:LocationData = cast(child, LocationData);
-			var id = location.id;
-			if(_id2location.exists(id)) {
-				throw new ArgumentError("A " + Type.typeof(child) + " with id = " + id + " already exists.");
-			}
-			_id2location.set(id, location);
-		}
-	}
-	
-	override function removeChildBefore(child:Data) {
-		super.removeChildBefore(child);
-		
-		if(child.is(LocationData)) {
-			var id = cast(child, LocationData).id;
-			if(_id2location.exists(id)) {
-				_id2location.remove(id);
-			}
-		}
+		_locations.readExternal(input);
 	}
 }
