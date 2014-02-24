63db61d86c67e078416bc57c2b1f74415933328e
slavara <ratdg@ya.ru>
2014-02-24 17:03:32 +0400

    -- add deserialize.

--- src/match3/core/models/LocationData.hx
+++ src/match3/core/models/LocationData.hx
@@ -1,4 +1,5 @@
 package match3.core.models;
+import slavara.haxe.core.events.models.DataBaseEvent;
 import slavara.haxe.game.Models.UnknownData;
 import slavara.haxe.game.Models.UnknownProto;
 using Reflect;
@@ -9,4 +10,20 @@ using Reflect;
 class LocationData extends UnknownData {
 
 	public function new(proto:UnknownProto) super(proto);
+	
+	public var isAvailable(default, null):Bool;
+	
+	private override function deserialize(input:Dynamic) {
+		super.deserialize(input);
+		
+		var changed = false;
+		if(input.hasField("is_available")) {
+			isAvailable = input.getProperty("is_available") == 1;
+			changed = true;
+		}
+		
+		if(changed && willTrigger(DataBaseEvent.CHANGE)) {
+			super.dispatchEvent(new DataBaseEvent(DataBaseEvent.CHANGE, true));
+		}
+	}
 }
