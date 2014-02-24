ae35aa443ad289603fbcc7cb2ba1aa8c9fe16366
slavara <ratdg@ya.ru>
2014-02-24 12:15:41 +0400

    set asset

--- src/match3/ccs/display/screens/WorldScreen.hx
+++ src/match3/ccs/display/screens/WorldScreen.hx
@@ -1,4 +1,5 @@
 package match3.ccs.display.screens;
+import match3.core.models.prototypes.WorldProto;
 import match3.core.models.WorldData;
 using slavara.haxe.core.utils.Utils.ValidateUtil;
 
@@ -12,12 +13,18 @@ class WorldScreen extends EmptyScreen {
 		this.data = data;
 	}
 	
+	override function render():Bool {
+		if(!super.render()) return false;
+		if(asset.isNull()) setAsset(cast(data.proto, WorldProto).asset);
+		
+		update();
+		return true;
+	}
+	
 	override function update() {
 		super.update();
-		if(stage.isNull() || data.isNull()) {
-			return;
-		}
+		if(stage.isNull() || data.isNull()) return;
 		
-		//draw locations
+		//CODE HERE
 	}
 }
