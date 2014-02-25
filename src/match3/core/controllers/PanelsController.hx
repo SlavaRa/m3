84f89efc468d366002c17b38e5efa8460331fa5f
slavara <ratdg@ya.ru>
2014-02-25 20:50:28 +0400

    create SettingsPanelController

--- src/match3/core/controllers/PanelsController.hx
+++ src/match3/core/controllers/PanelsController.hx
@@ -1,5 +1,6 @@
 package match3.core.controllers;
 import match3.core.controllers.panels.DebugPanelController;
+import match3.core.controllers.panels.SettingsPanelController;
 import slavara.haxe.core.controllers.BaseController.AbstractController;
 import slavara.haxe.core.controllers.BaseController.IController;
 
@@ -11,10 +12,11 @@ class PanelsController extends AbstractController {
 	public function new(controller:IController) super(controller);
 	
 	public var debug(default, null):DebugPanelController;
+	public var settings(default, null):SettingsPanelController;
 	
 	public override function initialize() {
 		super.initialize();
-		
 		debug = new DebugPanelController(baseController);
+		settings = new SettingsPanelController(baseController);
 	}
 }
