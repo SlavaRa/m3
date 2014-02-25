316fe8e6161cdc57e65a60dc4dfe1c67e215f17e
slavara <ratdg@ya.ru>
2014-02-25 20:33:17 +0400

    set data.

--- src/match3/ccs/display/screens/LocationScreen.hx
+++ src/match3/ccs/display/screens/LocationScreen.hx
@@ -1,9 +1,29 @@
 package match3.ccs.display.screens;
+import match3.core.models.LocationData;
+import match3.core.models.prototypes.LocationProto;
+import slavara.haxe.core.Errors.NotImplementedError;
+using slavara.haxe.core.Utils.ValidateUtil;
 
 /**
  * @author SlavaRa
  */
 class LocationScreen extends EmptyScreen {
 	
-	public function new() super();
+	public function new(data:LocationData) {
+		super();
+		this.data = data;
+	}
+	
+	override function render():Bool {
+		if(!super.render() return false;
+		if(asset.isNull()) setAsset(cast(data.proto, LocationProto).asset);
+		update();
+		return true;
+	}
+	
+	override private function update() {
+		super.update();
+		if(stage.isNull() || data.isNull()) return;
+		throw new NotImplementedError();
+	}
 }
