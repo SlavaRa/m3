b11cceab9a090ab233f53f13fc80d1c4813d1155
slavara <ratdg@ya.ru>
2014-02-25 20:32:40 +0400

    add WorldData::currentLocation.

--- src/match3/core/models/WorldData.hx
+++ src/match3/core/models/WorldData.hx
@@ -1,4 +1,5 @@
 package match3.core.models;
+import match3.core.models.LocationData;
 import match3.core.models.prototypes.WorldProto;
 import slavara.haxe.game.Models.DataCollection;
 import slavara.haxe.game.Models.UnknownProto;
@@ -11,6 +12,7 @@ class WorldData extends LocationData {
 	public function new(proto:UnknownProto) super(proto);
 	
 	public var locations(default, null):DataCollection<LocationData>;
+	public var currentLocation:LocationData;
 	
 	override function initialize() {
 		super.initialize();
