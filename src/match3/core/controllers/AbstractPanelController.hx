056bcb76d7c6ff975cc0741ca2a47e3cbf6479f6
slavara <ratdg@ya.ru>
2014-03-04 14:00:02 +0400

    create AbstractPanelController.

---
+++ src/match3/core/controllers/AbstractPanelController.hx
@@ -0,0 +1,27 @@
+package match3.core.controllers;
+import slavara.haxe.core.controllers.BaseController.AbstractController;
+import slavara.haxe.core.controllers.BaseController.IBaseController;
+import slavara.haxe.core.controllers.BaseController.IController;
+import slavara.haxe.core.Errors.NotImplementedError;
+import slavara.haxe.core.TypeDefs.DisplayObject;
+
+/**
+ * @author SlavaRa
+ */
+class AbstractPanelController extends AbstractController {
+
+	public function new(controller:IController) super(controller);
+	
+	var _view:DisplayObject;
+	
+	public override function initialize() {
+		super.initialize();
+		_view = Type.createInstance(getViewType(), []);
+		cast(baseController, IBaseController).container.addChild(_view);
+	}
+	
+	function getViewType():Class<DisplayObject> {
+		throw new NotImplementedError();
+		return null;
+	}
+}
