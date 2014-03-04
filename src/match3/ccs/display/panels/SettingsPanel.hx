0df21d004a6c53f7f349a68892da519cf54ebcfd
slavara <ratdg@ya.ru>
2014-03-04 15:35:40 +0400

    create SettingsPanel.

---
+++ src/match3/ccs/display/panels/SettingsPanel.hx
@@ -0,0 +1,10 @@
+package match3.ccs.display.panels;
+import slavara.haxe.core.TypeDefs.ResourceSprite;
+
+/**
+ * @author SlavaRa
+ */
+class SettingsPanel extends ResourceSprite {
+
+	public function new() super();
+}
