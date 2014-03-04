602aaaa261e357c3321c4b97979756e75b5c4f4a
slavara <ratdg@ya.ru>
2014-03-04 13:44:02 +0400

    create CCSPanelsController.

---
+++ src/match3/ccs/controllers/CCSPanelsController.hx
@@ -0,0 +1,11 @@
+package match3.ccs.controllers;
+import match3.core.controllers.PanelsController;
+import slavara.haxe.core.controllers.BaseController.IController;
+
+/**
+ * @author SlavaRa
+ */
+class CCSPanelsController extends PanelsController {
+	
+	public function new(controller:IController) super(controller);
+}
