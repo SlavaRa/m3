8125e82affe1ac903df10e468cf8e01e15f4223a
slavara <ratdg@ya.ru>
2014-03-04 13:17:55 +0400

    create SettingsPanelController.

---
+++ src/match3/ccs/display/panels/SettingsPanelController.hx
@@ -0,0 +1,16 @@
+package match3.ccs.display.panels;
+import slavara.haxe.core.controllers.BaseController.AbstractController;
+import slavara.haxe.core.controllers.BaseController.IController;
+
+/**
+ * @author SlavaRa
+ */
+class SettingsPanelController extends AbstractController {
+
+	public function new(controller:IController) super(controller);
+	
+	public override function initialize() {
+		super.initialize();
+		//TODO: implement me
+	}
+}
