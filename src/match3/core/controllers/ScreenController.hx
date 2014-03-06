463b70a5d674a63b38aacd0f359718aff037c4f0
slavara <ratdg@ya.ru>
2014-03-06 08:25:05 +0400

    cleanup.

--- src/match3/core/controllers/ScreenController.hx
+++ src/match3/core/controllers/ScreenController.hx
@@ -12,13 +12,12 @@ class ScreenController extends AbstractController {
 
 	public function new(controller:IController) super(controller);
 	
-	var _view:ScreenViewContainer;
-	
 	public override function initialize() {
 		super.initialize();
 		
 		var system:SystemController = cast(baseController, SystemController);
-		system.container.addChild(_view = new ScreenViewContainer(system.createScreenFactory()));
-		_view.data = cast(data, IStateMachineHolder);
+		var view = new ScreenViewContainer(system.createScreenFactory());
+		system.container.addChild(view);
+		view.data = cast(data, IStateMachineHolder);
 	}
 }
